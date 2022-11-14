c9e11346a7473a66052cca96de8cca726d1f46e3
HeloRodrigues <haraurelio@gmail.com>
2022-11-14 19:31:35 +0100

    Implement price and counter functionalities

--- index.js
+++ index.js
@@ -15,9 +15,7 @@ const addEventOnElement = (element, type, callback) => {
 // NAVBAR TOGGLE
 
 const navbar = document.querySelector("[data-navbar]");
-
 const navToggler = document.querySelectorAll("[data-nav-toggler]");
-
 const overlay = document.querySelector("[data-overlay]");
 
 const toggleNav = () => {
@@ -26,3 +24,95 @@ const toggleNav = () => {
 };
 
 addEventOnElement(navToggler, "click", toggleNav);
+
+// slider functionality
+
+const slider = document.querySelector("[data-slider]");
+const nextBtn = document.querySelector("[data-next]");
+const prevBtn = document.querySelector("[data-prev]");
+
+// set slider default position
+
+let sliderPosition = 0;
+
+// set number of total slider items
+
+const totalSliderItems = 4;
+
+//make next slide btn work
+
+const slideToNext = () => {
+  sliderPosition++;
+  slider.style.transform = `translateX(-${sliderPosition}00%)`;
+
+  disableSlideBtn();
+};
+
+addEventOnElement(nextBtn, "click", slideToNext);
+
+// make prev btn work
+
+const slideToPrev = () => {
+  sliderPosition--;
+  slider.style.transform = `translateX(-${sliderPosition}00%)`;
+
+  disableSlideBtn();
+};
+
+addEventOnElement(prevBtn, "click", slideToPrev);
+
+// Make next and prev btn disabled when slides finishes
+
+const disableSlideBtn = () => {
+  if (sliderPosition >= totalSliderItems - 1) {
+    nextBtn.classList.add("disabled");
+  } else {
+    nextBtn.classList.remove("disabled");
+  }
+  if (sliderPosition <= 0) {
+    prevBtn.classList.add("disabled");
+  } else {
+    prevBtn.classList.remove("disabled");
+  }
+};
+
+disableSlideBtn();
+
+//product quantity functionality
+
+const totalPriceElement = document.querySelector("[data-total-price]");
+const quantityElement = document.querySelector("[data-quantity]");
+const quantityMinusBtn = document.querySelector("[data-minus]");
+const quantityPlusBtn = document.querySelector("[data-plus]");
+
+// set product default quantity
+
+let quantity = 1;
+
+// set product defaul price
+
+let productPrice = 399;
+
+//set the initial price
+
+let totalPrice = 399;
+
+const increaseProductQuantity = () => {
+  quantity++;
+  totalPrice = quantity * productPrice;
+  quantityElement.textContent = quantity;
+  totalPriceElement.textContent = totalPrice;
+};
+addEventOnElement(quantityPlusBtn, "click", increaseProductQuantity);
+
+const decreaseProductQuantity = () => {
+  //   if (quantity > 1) {
+  //     quantity--;
+  //   }
+  quantity > 1 ? quantity-- : quantity;
+  totalPrice = quantity * productPrice;
+  quantityElement.textContent = quantity--;
+  totalPriceElement.textContent = totalPrice;
+};
+
+addEventOnElement(quantityMinusBtn, "click", decreaseProductQuantity);
